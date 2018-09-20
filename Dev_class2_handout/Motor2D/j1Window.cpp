928e9ca4f901bd19ce0742eabf1a39544bb07e23
lakaens <lakaens12@gmail.com>
2018-09-20 18:46:17 +0200

    dev class2 handout

---
+++ Dev_class2_handout/Motor2D/j1Window.cpp
@@ -0,0 +1,117 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Window.h"
+
+#include "SDL/include/SDL.h"
+
+
+j1Window::j1Window() : j1Module()
+{
+	window = NULL;
+	screen_surface = NULL;
+	name.create("window");
+}
+
+// Destructor
+j1Window::~j1Window()
+{
+}
+
+// Called before render is available
+bool j1Window::Awake()
+{
+	LOG("Init SDL window & surface");
+	bool ret = true;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		LOG("SDL_VIDEO could not initialize! SDL_Error: %s\n", SDL_GetError());
+		ret = false;
+	}
+	else
+	{
+		//Create window
+		Uint32 flags = SDL_WINDOW_SHOWN;
+
+		width = App->nodeconfig.child("window").child("WIDTH").attribute("width").as_int();
+		height = App->nodeconfig.child("window").child("HEIGHT").attribute("height").as_int();
+		scale = App->nodeconfig.child("window").child("SCALE").attribute("scale").as_int(1);
+
+
+
+		if(App->nodeconfig.child("window").child("FULLSCREEN").attribute("fullscreen").as_bool()==true)
+		{
+			flags |= SDL_WINDOW_FULLSCREEN;
+		}
+
+		if(App->nodeconfig.child("window").child("BORDERLESS").attribute("borderless").as_bool() == true)
+		{
+			flags |= SDL_WINDOW_BORDERLESS;
+		}
+
+		if(App->nodeconfig.child("window").child("RESIZABLE").attribute("resizable").as_bool() == false)
+		{
+			flags |= SDL_WINDOW_RESIZABLE;
+		}
+
+		if(App->nodeconfig.child("window").child("FULLSCREEN_WINDOW").attribute("fullscreen_window").as_bool() == true)
+		{
+			flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+		}
+
+		window = SDL_CreateWindow(App->nodeconfig.child("window").child("TITLE").attribute("title").as_string(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
+
+		if(window == NULL)
+		{
+			LOG("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			ret = false;
+		}
+		else
+		{
+			//Get window surface
+			screen_surface = SDL_GetWindowSurface(window);
+
+			// TODO 4: Read the title of the app from the XML
+			// and set directly the window title using SetTitle()
+			gamename = App->nodeconfig.child_value("name");
+			SetTitle(gamename);
+		}
+	}
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Window::CleanUp()
+{
+	LOG("Destroying SDL window and quitting all SDL systems");
+
+	//Destroy window
+	if(window != NULL)
+	{
+		SDL_DestroyWindow(window);
+	}
+
+	//Quit SDL subsystems
+	SDL_Quit();
+	return true;
+}
+
+// Set new window title
+void j1Window::SetTitle(const char* new_title)
+{
+	//title.create(new_title);
+	SDL_SetWindowTitle(window, new_title);
+}
+
+void j1Window::GetWindowSize(uint& width, uint& height) const
+{
+	width = this->width;
+	height = this->height;
+}
+
+uint j1Window::GetScale() const
+{
+	return scale;
+}
