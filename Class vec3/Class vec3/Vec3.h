425be465c80173c53d30cd15334421bcae4a6fa2
Eric Navarro de Santiago <ericnds@tr12.cat>
2018-09-26 10:16:58 +0200

    done again

--- Class vec3/Class vec3/Vec3.h
+++ Class vec3/Class vec3/Vec3.h
@@ -2,34 +2,33 @@
 #define VEC3
 #include <math.h>
 
-template <class Type>
+template <class TYPE>
 
 class vec3 {
 
 private:
-	Type x, y, z;
+	TYPE x, y, z;
 
 public:
 	vec3() {}
-	vec3(const vec3& vector) :x(x), y(y), z(z) {}
-
-	vec3(const Type& x, const Type& y, const Type& z) :x(x), y(y), z(z) {}
+	vec3(const vec3& vector) :x(vector.x), y(vector.y), z(vector.z) {}
+	vec3(const TYPE& x, const TYPE& y, const TYPE& z) :x(x), y(y), z(z) {}
 
 	vec3 normalize() const {
-		(Type)modul = sqrt(this->x*this->x + this->y*this->y + this->z*this->z);
+		(TYPE)modul = sqrt(this->x*this->x + this->y*this->y + this->z*this->z);
 		return (this->x / modul, this->y / modul, this->z / modul);
 	}
 	vec3 zero() {
-		this->x = (Type)0;
-		this->y = (Type)0;
-		this->z = (Type)0;
+		this->x = (TYPE)0;
+		this->y = (TYPE)0;
+		this->z = (TYPE)0;
 		return *this;
 	}
 	bool is_zero(const) {
-		return(this->x == (Type)0 && this->y == (Type)0 && this->z == (Type)0);
+		return(this->x == (TYPE)0 && this->y == (TYPE)0 && this->z == (TYPE)0);
 	}
-	vec3 distance_to(const vec3& vector) const{
-		return (Type) (sqrt((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z)));
+	float distance_to(const vec3& vector) const{
+		return (sqrtf((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z)));
 	}
 	vec3 operator +(const vec3& vector) const{
 		return(this->x + vector.x, this->y + vector.y, this->z + vector.z);
@@ -58,8 +57,8 @@ public:
 	vec3 operator ==(const vec3& vector) {
 		return (this->x == vector.x && this->y == vector.y && this->z == vector.z);
 	}
-	vec3 distance_to_square(const vec3& vector) const {
-		return (Type)((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z))*((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z));
+	float distance_to_squared(const vec3& vector) const {
+		return ((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z))*((vector.x * vector.x) + (vector.y * vector.y) + (vector.z * vector.z));
 	}
 
 };
