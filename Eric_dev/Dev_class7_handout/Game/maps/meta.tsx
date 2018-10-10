e3645cb632c11ed736a052c7b08cba1a562c0955
Eric Navarro de Santiago <ericnds@TR12.CAT>
2018-10-10 11:57:21 +0200

    hd7

---
+++ Eric_dev/Dev_class7_handout/Game/maps/meta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="meta" tilewidth="64" tileheight="32" spacing="1" margin="1" tilecount="1" columns="1">
+ <image source="meta.png" width="128" height="64"/>
+</tileset>
